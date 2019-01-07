91a1089795f0133c650f3d23d821333b4b2830cf
billta <17310857735@163.com>
2019-01-07 10:54:21 +0800

    Initial commit

---
+++ src/main/java/com/ld/channelview/MainActivity.java
@@ -0,0 +1,39 @@
+package com.ld.channelview;
+
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String channel = getChannel();
+        Toast.makeText(this,"渠道名"+channel,Toast.LENGTH_SHORT).show();
+
+    }
+
+
+    public void intoChannelView(View view) {
+        startActivity(new Intent(this, ChannelViewActivity.class));
+
+    }
+
+
+    private String getChannel() {
+        try {
+            PackageManager pm = getPackageManager();
+            ApplicationInfo appInfo = pm.getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+            return appInfo.metaData.getString("UMENG_CHANNEL");
+        } catch (PackageManager.NameNotFoundException ignored) {
+        }
+        return "";
+    }
+}
